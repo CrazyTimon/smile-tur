5f6443b465cea68fd3cd456dfa402aa53ca4fe5a
CrazyTimon <crazytimon88@gmail.com>
2013-03-06 22:35:59 +0600

    before programming

--- js/custom_script.js
+++ js/custom_script.js
@@ -1,6 +1,6 @@
 var animated_boxs = $(".hellow-box__caption").next();
 animated_boxs.hide();
-animated_boxs.slideDown(3000);
+animated_boxs.slideDown(1000);
 
 $(".hellow-box__caption").on("click", function(){
     var animated_boxs = $(this).next();
@@ -10,3 +10,14 @@ $(".hellow-box__caption").on("click", function(){
         animated_boxs.slideUp();
     }
 });
+$("#send_request").next().slideUp(1000);
+$( "#datepicker-start" ).datepicker();
+$( "#datepicker-end" ).datepicker();
+/*$("#send_request").next().slideUp(1000, function(){
+    $("#send_request").popover({
+        content:"Нажмите что бы раскрыть блок",
+        "title":"Подсказка",
+        trigger:"hover",
+        placement:"bottom"
+    }).popover("show");
+});*/
